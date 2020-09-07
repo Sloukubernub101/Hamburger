b6ed1182e0c4ca3b85c3f863e6fd9b14497a7977
Hilarry Henry <you@example.com>
2020-09-07 20:44:39 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+// const hamburger = document.getElementById('hamburger');
+// const navUL = document.getElementById('nav-ul');
+
+// hamburger.addEventListener('click', () => {
+
+// })
+
+const hamburger = document.getElementById('hamburger');
+const navUL = document.getElementById('nav-ul');
+
+hamburger.addEventListener('click',() => {
+      navUL.classList.toggle('open');
+});
